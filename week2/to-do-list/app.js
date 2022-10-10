ea55a837cf7d2428aabba67d45bce083f834f55c
wooyoung0830 <thddndud0830@gmail.com>
2022-10-10 10:53:54 +0900

    feat : addTodayTodo 기능

--- week2/to-do-list/app.js
+++ week2/to-do-list/app.js
@@ -9,3 +9,32 @@ const tomorrowTodoInput = document.querySelector('.tomorrow-to-do-input')
 const tomorrowTodoButton = document.querySelector('.tomorrow-input-button')
 const todayTodoList = document.querySelector('.today-todo-list')
 const tomorrowTodoList = document.querySelector('.tomorrow-todo-list')
+
+const addTodayTodo = () => {
+    if(todayTodoInput.value == ""){
+        alert("내용을 입력하세요!")
+    }else{
+        const currenLi = document.createElement("li");
+        currenLi.classList.add("todo-item")
+        const creatDeleteButton = document.createElement("span");
+    
+    
+        const currentInputData = todayTodoInput.value;
+        const plusToDoList = document.createElement("span");
+        plusToDoList.innerHTML= `${currentInputData}`
+        creatDeleteButton.className += " material-icons today-delete-button";
+        creatDeleteButton.innerHTML = 'delete'
+    
+        currenLi.appendChild(plusToDoList)
+        currenLi.appendChild(creatDeleteButton)
+    
+        todayTodoList.appendChild(currenLi)
+        todayTodoInput.value = "";
+    }
+}
+
+todayTodoButton.addEventListener("click", addTodayTodo)
+todayTodoInput.addEventListener("keydown", (e) => {
+    if(e.keyCode === 13) // enter 키 치면
+        addTodayTodo()
+}) 
