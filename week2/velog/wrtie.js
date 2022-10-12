ffe432ed81b079bdbb2c74e1eec251e930a5a0f1
wooyoung0830 <thddndud0830@gmail.com>
2022-10-12 11:09:42 +0900

    feat : 새로고침시 localStorage 불러오기 및 태그 중복 확인

--- week2/velog/wrtie.js
+++ week2/velog/wrtie.js
@@ -1,6 +1,5 @@
 const tagInput = document.querySelector(".tags-input");
 const tagList = document.querySelector('.tags-list');
-
 const tags = localStorage.getItem('tags') === null? [] : JSON.parse(localStorage.getItem('tags'));
 
 const saveLocalTags = (tag) => {
@@ -15,6 +14,16 @@ const removeLocalTags = (tag) => {
     localStorage.setItem('tags', JSON.stringify(tags));
 }
 
+const loadLocalstorage = () => {
+    tags.forEach((tagText)=>{
+        const tagItem = document.createElement('li');
+        tagItem.innerText = tagText;
+        tagItem.classList.add('tag-item');
+        tagItem.addEventListener('click', deleteTag);
+        tagList.appendChild(tagItem);
+    })
+}
+
 const deleteTag = (e) => {
     e.target.remove();
     removeLocalTags(e.target);
@@ -22,7 +31,9 @@ const deleteTag = (e) => {
 
 const addTag = () => {
     if(tagInput.value == ""){
-        alert('이미 존재하는 태그이거나 빈 태그입니다.');
+        alert('태그를 입력하세요!');
+    }else if( tags.includes(tagInput.value)){
+        alert('이미 존재하는 태그입니다!');
     }else{
         const tagItem = document.createElement('li');
         tagItem.innerText = `${tagInput.value}`
@@ -34,6 +45,7 @@ const addTag = () => {
     }
 }
 
+document.addEventListener("DOMContentLoaded", loadLocalstorage);
 tagInput.addEventListener('keydown', (e) => {
     if(e.keyCode === 13) // enter 키 치면
         addTag()
