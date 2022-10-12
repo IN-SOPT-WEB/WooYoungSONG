be11e40a7489eaa6092966bea12c8dc11660b20b
wooyoung0830 <thddndud0830@gmail.com>
2022-10-12 11:03:20 +0900

    feat : localStorage 저장 및 삭제 기능

--- week2/velog/wrtie.js
+++ week2/velog/wrtie.js
@@ -1,10 +1,24 @@
 const tagInput = document.querySelector(".tags-input");
 const tagList = document.querySelector('.tags-list');
 
-const deleteTag = (e) => {
-    e.target.remove();
+const tags = localStorage.getItem('tags') === null? [] : JSON.parse(localStorage.getItem('tags'));
+
+const saveLocalTags = (tag) => {
+    tags.push(tag);
+    localStorage.setItem('tags', JSON.stringify(tags));
 }
 
+const removeLocalTags = (tag) => {
+    let tags = JSON.parse(localStorage.getItem('tags'));
+    const tagText = tag.innerText;
+    tags.splice(tags.indexOf(tagText),1)
+    localStorage.setItem('tags', JSON.stringify(tags));
+}
+
+const deleteTag = (e) => {
+    e.target.remove();
+    removeLocalTags(e.target);
+}
 
 const addTag = () => {
     if(tagInput.value == ""){
@@ -15,6 +29,7 @@ const addTag = () => {
         tagItem.classList.add('tag-item');
         tagItem.addEventListener('click', deleteTag);
         tagList.appendChild(tagItem);
+        saveLocalTags(tagInput.value);
         tagInput.value='';
     }
 }
