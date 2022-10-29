75220afe34d8a3cb40ad5d7299bd8f38d08486f3
wooyoung0830 <thddndud0830@gmail.com>
2022-10-29 21:55:48 +0900

    feat : 정답/오답에 따라 modal 띄우기

---
+++ picture-game/src/Content/Modal.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const ModalBackground = styled.div`
+    width: 30rem;
+    height: 10rem;
+    position: absolute;
+    background: ${(props) => props.isCorrect ? "green" : "red"};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+const Result = styled.div`
+    font-size: 20px;
+`
+const CloseButton = styled.button`
+    background:white;
+    cursor: pointer;
+`
+export default function Modal({isCorrect, setIsModalOpen}) {
+    const closeModal = () => {
+        setIsModalOpen(false)
+        console.log("modal close")
+    }
+
+    return(
+        <ModalBackground isCorrect={isCorrect}>
+            <Result>{isCorrect ? "정답입니다!" : "오답입니다!"}</Result>
+            <CloseButton onClick={closeModal}>닫기</CloseButton>
+        </ModalBackground>
+    )
+}
--- picture-game/src/Content/Question.jsx
+++ picture-game/src/Content/Question.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components'
 import gameData from '../GameData.js';
+import Modal from './Modal.jsx';
 
 const QuizPicture = styled.img`
     width: 15rem;
@@ -16,11 +17,24 @@ const Answer = styled.button`
     width: 6rem;
     height: 2rem;
     font-size: 18px;
+    cursor: pointer;
 `
 
 export default function Question() {
     const [currentGameData, setCurrentGameData] = useState([...gameData][0]);
-    const chooseAnswer = () => {}
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const chooseAnswer = (e) => {
+        setIsModalOpen(true)
+        
+        if(e.target.innerText === currentGameData.correctAnswer){
+            setIsCorrect(true)
+        }else{
+            setIsCorrect(false)
+        }
+    }
+
     useEffect(() => {
         setCurrentGameData([...gameData][0])
     })
@@ -33,6 +47,7 @@ export default function Question() {
                     <Answer key={index} onClick={chooseAnswer}>{answer}</Answer>
                 ))}
             </Answers>
+            {isModalOpen && <Modal isCorrect={isCorrect} setIsModalOpen={setIsModalOpen}/>}
         </>
     )
 }
