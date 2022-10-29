d635f81a2d511657e121b0637de71041b7ea02e1
wooyoung0830 <thddndud0830@gmail.com>
2022-10-29 17:51:31 +0900

    add : yarn project 초기 세팅

---
+++ picture-game/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.div`
+    color:red;
+    font-size:14px;
+`
+
+export default function header() {
+    return (
+        <Title>header</Title>
+    )
+}
