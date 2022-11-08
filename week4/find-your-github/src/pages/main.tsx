7015e176566afcea267b4cceb7d84fba11fe172a
wooyoung0830 <thddndud0830@gmail.com>
2022-11-08 09:36:36 +0900

    feat : main page

--- week4/find-your-github/src/pages/main.tsx
+++ week4/find-your-github/src/pages/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 
@@ -8,17 +9,35 @@ const MainBackground = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   font-size: ${({ theme }) => theme.fontSizes.header};
   color: ${({ theme }) => theme.colors.mainColor};
+  background-color: ${({ theme }) => theme.colors.subColor};
+`;
+
+const SearchButton = styled.button`
+  color: ${({ theme }) => theme.colors.subColor};
+  background-color: ${({ theme }) => theme.colors.buttonColor};
+  border: none;
+  width: 10rem;
+  height: 5rem;
+  border-radius: 5px;
+  font-size: ${({ theme }) => theme.fontSizes.subTitle};
+  cursor: pointer;
 `;
 
 export default function Main() {
-  const [pokemons, setPokemons] = useState([]);
-  const [modalPokemon, setModalPokemon] = useState([0]);
-  const [point, setPoint] = useState(0);
-  const [isCorrect, setIsCorrect] = useState(false);
-  const [isModal, setIsModal] = useState(false);
+  const navigate = useNavigate();
 
-  return <MainBackground>main</MainBackground>;
+  const gotoSearch = () => {
+    navigate("/search");
+  };
+  return (
+    <>
+      <MainBackground>
+        GitHub Profile
+        <SearchButton onClick={gotoSearch}>검색하기</SearchButton>
+      </MainBackground>
+    </>
+  );
 }
